a3b90634d779a640ee18ad48b59e9cd8f2568009
SamarthAmrute <83336468+SamarthAmrute@users.noreply.github.com>
2021-09-06 16:06:29 +0530

    Add files via upload

---
+++ QUES01.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	char *subjects[7]={"Discrete Structures","Data Communication","DSA","Cloud Compuputing.","Application Development","Unix & Shell Modelling","Web Development"};
+	for (int i=0;i<7;i++)
+	cout<<subjects[i]<<"\n";
+	return 0;
+}
+
